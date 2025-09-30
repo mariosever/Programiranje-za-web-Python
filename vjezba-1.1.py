491bfb1ad2c0993efdbc8dbb84f1b65244aae646
Mario Sever <34382956+mariosever@users.noreply.github.com>
2025-09-30 11:30:26 +0200

    Add files via upload

---
+++ vjezba-1.1.py
@@ -0,0 +1,22 @@
+# Varijable i tipovi podataka
+
+# Cijeli brojevi (Integer)
+a = 8
+b = 3
+
+print(a)
+print(b)
+
+# Decimalni brojevi (Float)
+pi = 3.14
+x = 72.344
+
+print(pi)
+print(x)
+
+# Tekstualni (String)
+ime = "Ivo Ivić"
+poruka = "Pozdrav hakeri"
+
+print(ime)
+print(poruka)
